eb853032d09b37800029d75e835838678d08f087
Alejandro Gurovich <alejandro.gurovich@smart-sourcing.com.ar>
2020-02-06 11:49:30 -0300

    completed cells controller

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,2 +1,26 @@
 class CellsController < ApplicationController
+  #TODO: Add validations to prevent:
+    #1. indexing over a wrong game ID
+    #2. calling an update on an action that is not either 'CLICK' or 'FLAG'
+
+  # GET /games/1/cells
+  def index
+
+    game = Game.find(params[:game_id]).includes(:cells)
+
+    render json: user, status: :ok
+  end
+
+  # PUT /cells/1
+  #  params: { action: 'CLICK'|'FLAG' }
+  def udpate
+
+    cell = Cell.find(params[:id])
+
+    cell.send(params[:action])
+
+    game = cell.game.reload!
+
+    render json: game, include: :cells, status: :ok
+  end
 end
